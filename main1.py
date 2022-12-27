6aa542999d22893577e3bd2f6e5b50572beb6696
Tahmeed-programmer <79966762+Tahmeed-programmer@users.noreply.github.com>
2022-12-27 16:43:26 +0300

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,12 +1,40 @@
 # main.py
+
 from fastapi import FastAPI
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+from typing import Union
+
+class Item2(BaseModel):
+    text: str
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def hello():
     return {"message":"Hello TutLinks.com"}
 
-@app.get("/h5")
-def hello():
+@app.post("/h5/")
+def hello3(item:Item2):
     return {"message":" TutLinks.com"}
 
